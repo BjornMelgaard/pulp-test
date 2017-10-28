e0d77f8248e32d34f776698db33346275c514ce7
BjornMelgaard <melgaardbjorn@gmail.com>
2017-10-28 11:22:41 +0300

    ..

--- src/Main.purs
+++ src/Main.purs
@@ -8,4 +8,5 @@ import Data.Foldable (sum)
 isMultiple :: Int -> Boolean
 isMultiple n = mod n 3 == 0 || mod n 5 == 0
 
+answerA :: Int → Int → Int
 answerA min max = sum $ Array.filter isMultiple $ Array.range min max
